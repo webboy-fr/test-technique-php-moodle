9b9c828717d7f5ae9f6b1ceb608db7f8dbb429c8
fyzalis <julien.buabent@gmail.com>
2022-06-13 09:16:32 +0200

    initial commit

---
+++ exercice-1.php
@@ -0,0 +1,89 @@
+<?php
+
+function evaluate($expression){
+    // TODO : add rendering code here
+}
+
+// 100 + 200 + 300
+$expression1 = [
+    "type" => "add",
+    'children'=> [
+        [
+            "type" => "number",
+            "value"=>100
+        ],
+        [
+            "type" => "number",
+            "value"=> 200
+        ],
+        [
+            "type" => "number",
+            "value"=> 300
+        ]
+    ]
+];
+
+// 100 + 2 * (45 +5)
+$expression2 = [
+        "type" => "add",
+        'children'=> [
+                [
+                        "type" => "number",
+                        "value"=>100
+                ],
+                [
+                        "type" => "multiply",
+                        "children" =>[
+                                [
+                                        "type" => "number",
+                                        "value"=>2
+                                ],
+                                [
+                                        "type" => "add",
+                                        "children" =>[
+                                            [
+                                                "type" => "number",
+                                                "value"=>5
+                                            ],
+                                            [
+                                                "type" => "number",
+                                                "value"=>45
+                                            ]
+                        ]
+                                ]
+                        ]
+                ]
+        ]
+];
+
+
+
+// 1 + 100 / 1000
+$expression3 = [
+    "type" => "add",
+    'children'=> [
+        [
+            "type" => "number",
+            "value"=>1
+        ],
+        [
+            "type" => "fraction",
+            "top"=>
+                [
+                    "type" => "number",
+                    "value"=>100
+                ],
+            "bottom"=>
+                [
+                    "type" => "number",
+                    "value"=>1000
+                ]
+        ]
+    ]
+];
+
+echo "Expression 1 evaluates to: " . evaluate($expression1) . " <br>";
+
+echo "Expression 2 evaluates to: " . evaluate($expression2) . " <br>";
+
+echo "Expression 3 evaluates to: " . evaluate($expression3) . " <br>";
